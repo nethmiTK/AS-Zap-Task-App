16a8468216938e6ea6463546cce78dbf2a5f6f0d
nethmiTK <nethmitk33@gmail.com>
2025-04-05 16:29:55 +0530

    task ui

--- zaptask/src/App.jsx
+++ zaptask/src/App.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import TaskBitz from './pages/TaskBitz';
+ import TaskBitz from './pages/TaskBitz';
 import Zapd from './pages/Zapd';
 import './App.css';
-import Navbar from './components/NavBar';
-
+import TaskPage from './pages/TaskPage';
+ 
 export default function App() {
   return (
     <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
+       <Routes>
+        <Route path="/" element={<TaskPage />} />
         <Route path="/task-bitz" element={<TaskBitz />} />
         <Route path="/zapd" element={<Zapd />} />
       </Routes>
