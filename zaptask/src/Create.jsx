d900b9cfaf1f8d6c78bdbb827413e3f2c062f31f
nethmiTK <nethmitk33@gmail.com>
2025-04-06 12:59:40 +0530

    updated

--- zaptask/src/Create.jsx
+++ zaptask/src/Create.jsx
@@ -1,24 +1,33 @@
-import React,{ useState } from "react";  
+import React, { useState } from "react";
 import axios from "axios";
-function Create() {
-    const [task, setTask] = useState();
-    const handleAdd=() => {
-        axios.post('http://localhost:3001/add', { task: task })
-            .then(result => 
-                console.log(result))
-                .catch(err=> console.log(err))
-            
 
-    }
-    return (
-      <div className="create_form">
-         <input type="text" id="" 
-          placeholder="Enter Task" onChange={(e)=>setTask(e.target.value)} />
-<button type="button" onClick={handleAdd}>+ Add Task</button>
-         
-         
-      </div>
-    )
-     
+function Create({ refreshTodos }) {
+  const [task, setTask] = useState("");
+
+  const handleAdd = () => {
+    if (!task.trim()) return;
+    axios
+      .post("http://localhost:3001/add", { task: task })
+      .then(() => {
+        setTask("");
+        refreshTodos(); // refresh the todo list without reloading the page
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className="create_form">
+      <input
+        type="text"
+        placeholder="Enter Task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button type="button" onClick={handleAdd}>
+        + Add Task
+      </button>
+    </div>
+  );
 }
+
 export default Create;
--- zaptask/src/Home.jsx
+++ zaptask/src/Home.jsx
@@ -1,31 +1,63 @@
- import react, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Create from "./Create";
-import React, { useState } from "react";
 import axios from "axios";
- function Home() {
-    const[todos, setTodos] = useState([]);
-    useEffect(() => {
-      axios.get('http://localhost:3001/get')
-         .then(result => setTodos(result.data))
-            .catch(err => console.log(err))
+import { BsCircleFill } from "react-icons/bs";
 
-         },[])
- 
+function Home() {
+  const [todos, setTodos] = useState([]);
 
-    return (
-      <div className="bg-pink-100 min-h-screen p-4">
-                     <h2>Zap task</h2>
+  const fetchTodos = () => {
+    axios
+      .get("http://localhost:3001/get")
+      .then((res) => setTodos(res.data))
+      .catch((err) => console.log(err));
+  };
 
-             <Create/>
-             {
-                todos.length === 0 ? 
-                    <div className="text-gray-500">No tasks available</div>:
-             todos.map(todo => (<div>
-                {todo.task}
-      </div>
-    ))
- }
- </div>
-    );
-  }
- export default Home;
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const handleEdit = (id) => {
+    axios
+      .put(`http://localhost:3001/update/${id}`)
+      .then(() => fetchTodos())
+      .catch((err) => console.log(err));
+  };
+
+  const handleDelete = (id) => {
+    axios
+      .delete(`http://localhost:3001/delete/${id}`)
+      .then(() => fetchTodos())
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className="bg-pink-100 min-h-screen p-4">
+      <h2>Zap Task</h2>
+      <Create refreshTodos={fetchTodos} />
+
+      {todos.length === 0 ? (
+        <h2>No tasks available</h2>
+      ) : (
+        todos.map((todo) => (
+          <div className="task" key={todo._id}>
+            <div className="check" onClick={() => handleEdit(todo._id)}>
+              <BsCircleFill className="icon" />
+              <p className={todo.done ? "line-through" : ""}>{todo.task}</p>
+            </div>
+            <div>
+              <span>
+                <BsCircleFill
+                  className="icon"
+                  onClick={() => handleDelete(todo._id)}
+                />
+              </span>
+            </div>
+          </div>
+        ))
+      )}
+    </div>
+  );
+}
+
+export default Home;
